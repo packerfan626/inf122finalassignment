f028c4544f9a9756341cbfb9d24159a80084c5a0
cpoon4 <poonch@uci.edu>
2017-03-15 21:49:28 -0700

    test

--- Inf122Games/src/Server/ServerGUI.java
+++ Inf122Games/src/Server/ServerGUI.java
@@ -1,12 +1,35 @@
 package Server;
 
-public class ServerGUI{
-	private static Server server;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.Color;
 
-	public static void main(String[] args) {
+public class ServerGUI extends JFrame
+{
+	private static int port = 4444;
+	private static Server server;
+	
+	public ServerGUI() {
+		super("Game Server");
+		getContentPane().setLayout(null);
+		
+		JTextArea txtrServerOnline = new JTextArea();
+		txtrServerOnline.setForeground(new Color(0, 153, 0));
+		txtrServerOnline.setText("Server Online");
+		txtrServerOnline.setBounds(164, 35, 108, 22);
+		getContentPane().add(txtrServerOnline);
+		
+		JTextArea taPort = new JTextArea();
+		taPort.setBounds(185, 79, 73, 22);
+		getContentPane().add(taPort);
+		taPort.setText("Port use: " + port);
+	}	
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-		server = new Server(4444);
+		server = new Server(port);
 		server.startServer();
 	}
-
 }
